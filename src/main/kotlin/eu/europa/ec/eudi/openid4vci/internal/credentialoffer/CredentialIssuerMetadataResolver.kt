8ed8be532191a5084448d39fe427001b87a992ff
Dimitris ZARRAS <Dimitris.ZARRAS@netcompany.com>
2023-10-02 15:55:37 +0300

    Make CredentialIssuerMetadataResolver an internal class to be used by DefaultCredentialOfferRequestResolver.

--- src/main/kotlin/eu/europa/ec/eudi/openid4vci/internal/credentialoffer/CredentialIssuerMetadataResolver.kt
+++ src/main/kotlin/eu/europa/ec/eudi/openid4vci/internal/credentialoffer/CredentialIssuerMetadataResolver.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package eu.europa.ec.eudi.openid4vci.internal.credentialissuer
+package eu.europa.ec.eudi.openid4vci.internal.credentialoffer
 
 import com.nimbusds.jose.EncryptionMethod
 import com.nimbusds.jose.JWEAlgorithm
@@ -25,12 +25,141 @@ import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.*
+import java.io.Serializable
 import java.net.URL
+
+/**
+ * Errors that can occur while trying to fetch and validate the metadata of a Credential Issuer.
+ */
+sealed interface CredentialIssuerMetadataError : Serializable {
+
+    /**
+     * Indicates the Credential Issuer metadata could not be fetched.
+     */
+    data class UnableToFetchCredentialIssuerMetadata(val cause: Throwable) : CredentialIssuerMetadataError
+
+    /**
+     * Indicates the Credential Issuer metadata could not be parsed.
+     */
+    data class NonParseableCredentialIssuerMetadata(val cause: Throwable) : CredentialIssuerMetadataError
+
+    /**
+     * Wraps this [CredentialIssuerMetadataError] to a [CredentialIssuerMetadataException].
+     */
+    fun toException(): CredentialIssuerMetadataException = CredentialIssuerMetadataException(this)
+
+    /**
+     * Wraps this [CredentialIssuerMetadataError] and throws it as a [CredentialIssuerMetadataException].
+     */
+    fun raise(): Nothing = throw toException()
+}
+
+/**
+ * Errors that can occur while trying to to validate the metadata of a Credential Issuer.
+ */
+sealed interface CredentialIssuerMetadataValidationError : CredentialIssuerMetadataError {
+
+    /**
+     * The Id of the Credential Issuer is not valid.
+     */
+    data class InvalidCredentialIssuerId(val reason: Throwable) : CredentialIssuerMetadataValidationError
+
+    /**
+     * The URL of the Authorization Server is not valid.
+     */
+    data class InvalidAuthorizationServer(val reason: Throwable) : CredentialIssuerMetadataValidationError
+
+    /**
+     * The URL of the Credential Endpoint is not valid.
+     */
+    data class InvalidCredentialEndpoint(val reason: Throwable) : CredentialIssuerMetadataValidationError
+
+    /**
+     * The URL of the Batch Credential Endpoint is not valid.
+     */
+    data class InvalidBatchCredentialEndpoint(val reason: Throwable) : CredentialIssuerMetadataValidationError
+
+    /**
+     * The URL of the Deferred Credential Endpoint is not valid.
+     */
+    data class InvalidDeferredCredentialEndpoint(val reason: Throwable) : CredentialIssuerMetadataValidationError
+
+    /**
+     * The supported Credential Encryption Algorithms are not valid.
+     */
+    data class InvalidCredentialResponseEncryptionAlgorithmsSupported(val reason: Throwable) :
+        CredentialIssuerMetadataValidationError
+
+    /**
+     * The supported Credential Encryption Methods are not valid.
+     */
+    data class InvalidCredentialResponseEncryptionMethodsSupported(val reason: Throwable) :
+        CredentialIssuerMetadataValidationError
+
+    /**
+     * Credential Encryption Algorithms are required.
+     */
+    object CredentialResponseEncryptionAlgorithmsRequired : CredentialIssuerMetadataValidationError {
+
+        private fun readResolve(): Any = CredentialResponseEncryptionAlgorithmsRequired
+
+        override fun toString(): String = "CredentialResponseEncryptionAlgorithmsRequired"
+    }
+
+    /**
+     * The supported Credentials not valid.
+     */
+    data class InvalidCredentialsSupported(val reason: Throwable) : CredentialIssuerMetadataValidationError
+
+    /**
+     * Supported Credentials are required.
+     */
+    object CredentialsSupportedRequired : CredentialIssuerMetadataValidationError {
+
+        private fun readResolve(): Any = CredentialsSupportedRequired
+
+        override fun toString(): String = "CredentialsSupportedRequired"
+    }
+
+    /**
+     * Display is not valid.
+     */
+    data class InvalidDisplay(val reason: Throwable) : CredentialIssuerMetadataValidationError
+}
+
+/**
+ * Indicates a [CredentialOfferRequestError] occurred while trying to fetch or validate the metadata of a Credential Issuer.
+ */
+data class CredentialIssuerMetadataException(val error: CredentialIssuerMetadataError) : Exception()
+
+/**
+ * Service for fetching, parsing, and validating the metadata of a Credential Issuer.
+ */
+internal fun interface CredentialIssuerMetadataResolver {
+
+    /**
+     * Tries to fetch and validate the metadata of a Credential Issuer.
+     */
+    suspend fun resolve(issuer: CredentialIssuerId): Result<CredentialIssuerMetadata>
+
+    companion object {
+
+        /**
+         * Creates a new [CredentialIssuerMetadataResolver] instance.
+         *
+         * [httpGet] execution are dispatched on [ioCoroutineDispatcher].
+         */
+        operator fun invoke(
+            ioCoroutineDispatcher: CoroutineDispatcher,
+            httpGet: HttpGet<String>,
+        ): CredentialIssuerMetadataResolver = DefaultCredentialIssuerMetadataResolver(ioCoroutineDispatcher, httpGet)
+    }
+}
 
 /**
  * Default implementation of [CredentialIssuerMetadataResolver].
  */
-internal class DefaultCredentialIssuerMetadataResolver(
+private class DefaultCredentialIssuerMetadataResolver(
     private val ioCoroutineDispatcher: CoroutineDispatcher = Dispatchers.IO,
     private val httpGet: HttpGet<String>,
 ) : CredentialIssuerMetadataResolver {
@@ -175,7 +304,9 @@ internal class DefaultCredentialIssuerMetadataResolver(
                     this,
                 )
 
-                "mso_mdoc" -> Json.decodeFromJsonElement<MsoMdocCredentialSupportedObject>(this)
+                "mso_mdoc" -> Json.decodeFromJsonElement<MsoMdocCredentialSupportedObject>(
+                    this,
+                )
 
                 else -> throw IllegalArgumentException("Unsupported Credential format '$format'")
             }
