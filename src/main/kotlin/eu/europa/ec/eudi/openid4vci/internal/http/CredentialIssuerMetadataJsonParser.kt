d85625e24e99ebf631efc082afd6869f11bdd4fe
Jose Luis Crisostomo <jl.cs606@gmail.com>
2025-01-15 17:09:39 +0100

    fixed to 5

--- src/main/kotlin/eu/europa/ec/eudi/openid4vci/internal/http/CredentialIssuerMetadataJsonParser.kt
+++ src/main/kotlin/eu/europa/ec/eudi/openid4vci/internal/http/CredentialIssuerMetadataJsonParser.kt
@@ -364,7 +364,7 @@ private data class W3CSignedJwtCredentialTO(
 
 @Serializable
 private data class BatchCredentialIssuanceTO(
-    @SerialName("batch_size") @Required val batchSize: Int,
+    @SerialName("batch_size") @Required val batchSize: Int = 5,
 )
 
 /**
