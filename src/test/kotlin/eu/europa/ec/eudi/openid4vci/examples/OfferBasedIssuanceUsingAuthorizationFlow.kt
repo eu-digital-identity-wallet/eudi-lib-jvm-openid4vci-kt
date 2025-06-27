fad1b838227fee683412bf515b4c4132c49308ad
Dimitris ZARRAS <Dimitris.ZARRAS@netcompany.com>
2025-06-27 12:11:52 +0300

    Add test cases for EHIC in SD-JWT VC compact

--- src/test/kotlin/eu/europa/ec/eudi/openid4vci/examples/OfferBasedIssuanceUsingAuthorizationFlow.kt
+++ src/test/kotlin/eu/europa/ec/eudi/openid4vci/examples/OfferBasedIssuanceUsingAuthorizationFlow.kt
@@ -25,7 +25,8 @@ fun main(): Unit = runBlocking {
         "credential_issuer%22:%22https://dev.issuer-backend.eudiw.dev%22,%22" +
         "credential_configuration_ids%22:[%22${PidDevIssuer.PID_MsoMdoc_config_id.value}%22," +
         "%22${PidDevIssuer.PID_SdJwtVC_config_id.value}%22,%22${PidDevIssuer.MDL_config_id.value}%22," +
-        "%22${URLEncoder.encode(PidDevIssuer.EHIC_config_id.value, Charsets.UTF_8)}%22]," +
+        "%22${URLEncoder.encode(PidDevIssuer.EHIC_JwsJson_config_id.value, Charsets.UTF_8)}%22," +
+        "%22${URLEncoder.encode(PidDevIssuer.EHIC_Compact_config_id.value, Charsets.UTF_8)}%22]," +
         "%22grants%22:%7B%22authorization_code%22:%7B%22" +
         "authorization_server%22:%22https://dev.authenticate.eudiw.dev/realms/pid-issuer-realm%22%7D%7D%7D"
 
--- src/test/kotlin/eu/europa/ec/eudi/openid4vci/examples/PidDevIssuerTest.kt
+++ src/test/kotlin/eu/europa/ec/eudi/openid4vci/examples/PidDevIssuerTest.kt
@@ -51,9 +51,18 @@ class PidDevIssuerTest {
     }
 
     @Test @Ignore
-    fun `Issue EHIC in sd-jwt vc using authorize code flow and JWT proofs`() = runTest {
+    fun `Issue EHIC in sd-jwt vc jws json flattened using authorize code flow and JWT proofs`() = runTest {
         PidDevIssuer.testIssuanceWithAuthorizationCodeFlow(
-            PidDevIssuer.EHIC_config_id,
+            PidDevIssuer.EHIC_JwsJson_config_id,
+            enableHttpLogging = false,
+            batchOption = BatchOption.Specific(2),
+        )
+    }
+
+    @Test @Ignore
+    fun `Issue EHIC in sd-jwt vc compact using authorize code flow and JWT proofs`() = runTest {
+        PidDevIssuer.testIssuanceWithAuthorizationCodeFlow(
+            PidDevIssuer.EHIC_Compact_config_id,
             enableHttpLogging = false,
             batchOption = BatchOption.Specific(2),
         )
