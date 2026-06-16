e88e6f4f6da5fe8159293680ddbbe0a0ee277fc8
Dimitris ZARRAS <Dimitris.ZARRAS@netcompany.com>
2026-06-16 14:32:31 +0300

    Rename and document default value for ProofsConfig

--- src/test/kotlin/eu/europa/ec/eudi/openid4vci/examples/PidDevIssuer.kt
+++ src/test/kotlin/eu/europa/ec/eudi/openid4vci/examples/PidDevIssuer.kt
@@ -47,7 +47,7 @@ internal object PidDevIssuer :
         authorizeIssuanceConfig = AuthorizeIssuanceConfig.FAVOR_SCOPES,
         parUsage = ParUsage.Required,
         supportedCredentialReusePolicies = CredentialReusePolicies.Supported(setOf(EudiReusePolicyType.OnceOnly)),
-        proofs = ProofsConfig.ETSI119472Part3,
+        proofs = ProofsConfig.Default,
     )
 
     val PID_SdJwtVC_config_id = CredentialConfigurationIdentifier("eu.europa.ec.eudi.pid_vc_sd_jwt")
